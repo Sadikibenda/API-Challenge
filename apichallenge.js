aee61c4bd03abb70133f17276983733e29e273f9
Sadikibenda <Sadiki.benda1@gmail.com>
2023-12-12 18:06:41 -0500

    update

--- apichallenge.js
+++ apichallenge.js
@@ -11,6 +11,7 @@ button.addEventListener('click', function(){
     //console.log(json)
 
     let search = json.results
+    Berries.innerHTML = '';        // used this line of code to get response once after click!!! or use  { once: true } in the last line.
     for (p of search){
         //console.log(p)
         let listItem = document.createElement('li')
@@ -19,24 +20,5 @@ button.addEventListener('click', function(){
 
     }
 })
-})
+})  
 
-
-
-
-
-/*fetch("https://pokeapi.co/api/v2/berry-flavor/")
-.then(response => response.json())
-
-.then(function(json) {
-    //console.log(json)
-
-    let search = json.results
-    for (p of search){
-        //console.log(p)
-        let listItem = document.createElement('li')
-        listItem.innerHTML = p.name
-        Berries.appendChild(listItem)
-
-    }
-})*/
